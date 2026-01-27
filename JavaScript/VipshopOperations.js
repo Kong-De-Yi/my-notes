8b9b658cf38aed86043cde2746c00ea3b58abdfd
Kong-De-Yi <kdeyi@163.com>
2026-01-27 20:09:09 +0800

    26.01.27 20:09

--- JavaScript/VipshopOperations.js
+++ JavaScript/VipshopOperations.js
@@ -1,5 +1,5 @@
 class Main {
-  static updateRegularProduct() {
+  static updateRegularProduct(updateWB = true) {
     try {
       VipshopGoods.initializeData();
       RegularProduct.initializeData();
@@ -7,15 +7,6 @@ class Main {
       MsgBox(e.message);
       return;
     }
-
-    RegularProduct.data.forEach((item) => {
-      let findItem = VipshopGoods.data.find(
-        (key) => key.itemNumber === item.itemNumber,
-      );
-      if (!findItem) {
-        VipshopGoods.data.push(new VipshopGoods(item.itemNumber));
-      }
-    });
 
     let absentItemNumbers = [];
     VipshopGoods.data.forEach((item) => {
@@ -36,6 +27,15 @@ class Main {
       );
       return;
     }
+
+    RegularProduct.data.forEach((item) => {
+      let findItem = VipshopGoods.data.find(
+        (key) => key.itemNumber === item.itemNumber,
+      );
+      if (!findItem) {
+        VipshopGoods.data.push(new VipshopGoods(item.itemNumber));
+      }
+    });
 
     VipshopGoods.data.forEach((item) => {
       let findItem = RegularProduct.data.find(
@@ -77,20 +77,24 @@ class Main {
         .join("/");
     });
 
-    DAO.updateWorksheet(
-      VipshopGoods.wsName,
-      VipshopGoods.data,
-      Object.assign(
-        {},
-        VipshopGoods.keyToTitle,
-        VipshopGoods.optionalKeyToTitle,
-      ),
-    );
+    if (updateWB) {
+      DAO.updateWorksheet(
+        VipshopGoods.wsName,
+        VipshopGoods.data,
+        Object.assign(
+          {},
+          VipshopGoods.keyToTitle,
+          VipshopGoods.optionalKeyToTitle,
+        ),
+      );
+
+      MsgBox("【" + RegularProduct.wsName + "】" + "更新成功！");
+    }
   }
 
-  static updateProductPrice() {
+  static updateProductPrice(updateWB = true) {
     try {
-      this.updateRegularProduct();
+      this.updateRegularProduct(false);
       ProductPrice.initializeData();
     } catch (e) {
       MsgBox(e.message);
@@ -121,7 +125,7 @@ class Main {
       let findItem = ProductPrice.data.find(
         (key) => key.itemNumber === item.itemNumber,
       );
-
+      item.designNumber = findItem.designNumber;
       item.picture = findItem.picture;
       item.costPrice = findItem.costPrice;
       item.lowestPrice = findItem.lowestPrice;
@@ -130,20 +134,24 @@ class Main {
       item.userOperations2 = findItem.userOperations2;
     });
 
-    DAO.updateWorksheet(
-      VipshopGoods.wsName,
-      VipshopGoods.data,
-      Object.assign(
-        {},
-        VipshopGoods.keyToTitle,
-        VipshopGoods.optionalKeyToTitle,
-      ),
-    );
+    if (updateWB) {
+      DAO.updateWorksheet(
+        VipshopGoods.wsName,
+        VipshopGoods.data,
+        Object.assign(
+          {},
+          VipshopGoods.keyToTitle,
+          VipshopGoods.optionalKeyToTitle,
+        ),
+      );
+
+      MsgBox("【" + ProductPrice.wsName + "】" + "更新成功！");
+    }
   }
 
   static updateInventory() {
     try {
-      this.updateRegularProduct();
+      this.updateRegularProduct(false);
       ComboProduct.initializeData();
       Inventory.initializeData();
     } catch (e) {
@@ -159,6 +167,14 @@ class Main {
       item.finishedGoodsPrepareInventory = 0;
       item.finishedGoodsReturnInventory = 0;
       item.finishedGoodsPurchaseInventory = 0;
+
+      item.generalGoodsMainInventory = 0;
+      item.generalGoodsIncomingInventory = 0;
+      item.generalGoodsFinishingInventory = 0;
+      item.generalGoodsOversoldInventory = 0;
+      item.generalGoodsPrepareInventory = 0;
+      item.generalGoodsReturnInventory = 0;
+      item.generalGoodsPurchaseInventory = 0;
 
       let products = RegularProduct.data.filter(
         (key) => item.itemNumber == key.itemNumber,
@@ -183,17 +199,66 @@ class Main {
           this.finishedGoodsPurchaseInventory +=
             +findProuctInventory.purchaseInventory;
         }
-      });
-      item.finishedGoodsTotalInventory =
-        item.finishedGoodsMainInventory +
-        item.finishedGoodsIncomingInventory +
-        item.finishedGoodsFinishingInventory +
-        item.finishedGoodsOversoldInventory +
-        item.finishedGoodsPrepareInventory +
-        item.finishedGoodsReturnInventory +
-        item.finishedGoodsPurchaseInventory;
+        item.finishedGoodsTotalInventory =
+          item.finishedGoodsMainInventory +
+          item.finishedGoodsIncomingInventory +
+          item.finishedGoodsFinishingInventory +
+          item.finishedGoodsOversoldInventory +
+          item.finishedGoodsPrepareInventory +
+          item.finishedGoodsReturnInventory +
+          item.finishedGoodsPurchaseInventory;
 
-      //通货库存计算
+        //通货库存计算
+        let findComboProducts = ComboProduct.data.filter(
+          (key) =>
+            key.productCode == productItem.productCode &&
+            !key.subProductCode.startsWith("YH") &&
+            !key.subProductCode.startsWith("FL"),
+        );
+
+        if (findComboProducts.length != 0) {
+          findComboProducts.forEach((comboProduct) => {
+            let findSubProductInventory = Inventory.data.find(
+              (key) => key.productCode == comboProduct.subProductCode,
+            );
+            if (findSubProductInventory) {
+              item.generalGoodsMainInventory +=
+                +findSubProductInventory.mainInventory /
+                +comboProduct.subProductQuantity;
+              item.generalGoodsIncomingInventory +=
+                +findSubProductInventory.incomingInventory /
+                +comboProduct.subProductQuantity;
+              item.generalGoodsFinishingInventory +=
+                +findSubProductInventory.finishingInventory /
+                +comboProduct.subProductQuantity;
+              item.generalGoodsOversoldInventory +=
+                +findSubProductInventory.oversoldInventory /
+                +comboProduct.subProductQuantity;
+              item.generalGoodsPrepareInventory +=
+                +findSubProductInventory.prepareInventory /
+                +comboProduct.subProductQuantity;
+              item.generalGoodsReturnInventory +=
+                +findSubProductInventory.returnInventory /
+                +comboProduct.subProductQuantity;
+              item.generalGoodsPurchaseInventory +=
+                +findSubProductInventory.purchaseInventory /
+                +comboProduct.subProductQuantity;
+            }
+          });
+        }
+
+        item.generalGoodsTotalInventory =
+          item.generalGoodsMainInventory +
+          item.generalGoodsIncomingInventory +
+          item.generalGoodsFinishingInventory +
+          item.generalGoodsOversoldInventory +
+          item.generalGoodsPrepareInventory +
+          item.generalGoodsReturnInventory +
+          item.generalGoodsPurchaseInventory;
+      });
+
+      item.totalInventory =
+        item.finishedGoodsTotalInventory + item.generalGoodsTotalInventory;
     });
 
     DAO.updateWorksheet(
@@ -205,6 +270,126 @@ class Main {
         VipshopGoods.optionalKeyToTitle,
       ),
     );
+
+    MsgBox("【" + Inventory.wsName + "】" + "更新成功！");
+  }
+
+  static updateProductSales() {
+    try {
+      this.updateRegularProduct(false);
+      ProductSales.initializeData();
+    } catch (e) {
+      MsgBox(e.message);
+      return;
+    }
+
+    let today = new Date();
+    let startDate = new Date();
+    let endDate = new Date();
+    startDate.setDate(today.getDate() - 7);
+    endDate.setDate(today.getDate() - 1);
+
+    let startDateYear = startDate.getFullYear();
+    let startDateMonth = String(startDate.getMonth() + 1).padStart(2, "0");
+    let startDateDay = String(startDate.getDate()).padStart(2, "0");
+    let endDateYear = endDate.getFullYear();
+    let endDateMonth = String(endDate.getMonth() + 1).padStart(2, "0");
+    let endDateDay = String(endDate.getDate()).padStart(2, "0");
+
+    let dateOfLast7Days = `${startDateYear}-${startDateMonth}-${startDateDay}~${endDateYear}-${endDateMonth}-${endDateDay}`;
+
+    let styleSalesOfLast7DaysMap = new Map();
+
+    VipshopGoods.data.forEach((item) => {
+      let findItem = ProductSales.data.find(
+        (key) =>
+          key.itemNumber == item.itemNumber && key.salesDate == dateOfLast7Days,
+      );
+
+      if (findItem) {
+        item.exposureUVOfLast7Days = +findItem.exposureUV;
+        item.productDetailsUVOfLast7Days = +findItem.productDetailsUV;
+        item.addToCartUVOfLast7Days = +findItem.addToCartUV;
+        item.customerCountOfLast7Days = +findItem.customerCount;
+        item.rejectAndReturnCountOfLast7Days = +findItem.rejectAndReturnCount;
+        item.salesQuantityOfLast7Days = +findItem.salesQuantity;
+        item.salesAmountOfLast7Days = +findItem.salesAmount;
+
+        item.unitPriceOfLast7Days =
+          item.salesQuantityOfLast7Days == 0
+            ? ""
+            : item.salesAmountOfLast7Days / item.salesQuantityOfLast7Days;
+        item.clickThroughRateOfLast7Days =
+          item.exposureUVOfLast7Days == 0
+            ? ""
+            : item.productDetailsUVOfLast7Days / item.exposureUVOfLast7Days;
+        item.addToCartRateOfLast7Days =
+          item.productDetailsUVOfLast7Days == 0
+            ? ""
+            : item.addToCartUVOfLast7Days / item.productDetailsUVOfLast7Days;
+        item.purchaseRateOfLast7Days =
+          item.productDetailsUVOfLast7Days == 0
+            ? ""
+            : item.customerCountOfLast7Days / item.productDetailsUVOfLast7Days;
+        item.rejectAndReturnRateOfLast7Days =
+          item.salesQuantityOfLast7Days == 0
+            ? ""
+            : item.rejectAndReturnCountOfLast7Days /
+              item.salesQuantityOfLast7Days;
+
+        //近7天款销量
+        let styleSalesOfLast7Days = styleSalesOfLast7DaysMap.get(
+          item.styleNumber,
+        );
+
+        if (!styleSalesOfLast7Days) {
+          styleSalesOfLast7DaysMap.set(
+            item.styleNumber,
+            +findItem.salesQuantity,
+          );
+        } else {
+          styleSalesOfLast7DaysMap.set(
+            item.styleNumber,
+            styleSalesOfLast7Days + +findItem.salesQuantity,
+          );
+        }
+
+        item.firstListingTime = findItem.firstListingTime
+          ? "'" + findItem.firstListingTime
+          : "";
+      }
+
+      for (let prop of Object.keys(VipshopGoods.optionalKeyToTitle)) {
+        let findItem = ProductSales.data.find(
+          (key) =>
+            key.itemNumber == item.itemNumber && "+" + key.salesDate == prop,
+        );
+
+        if (findItem) {
+          item[prop] = findItem.salesQuantity;
+          item.firstListingTime = findItem.firstListingTime
+            ? "'" + findItem.firstListingTime
+            : "";
+        }
+      }
+    });
+
+    VipshopGoods.data.forEach((item) => {
+      item.styleSalesOfLast7Days =
+        styleSalesOfLast7DaysMap.get(item.styleNumber) ?? 0;
+    });
+
+    DAO.updateWorksheet(
+      VipshopGoods.wsName,
+      VipshopGoods.data,
+      Object.assign(
+        {},
+        VipshopGoods.keyToTitle,
+        VipshopGoods.optionalKeyToTitle,
+      ),
+    );
+
+    MsgBox("【" + ProductSales.wsName + "】" + "更新成功！");
   }
 }
 //货号总表
@@ -223,6 +408,7 @@ class VipshopGoods {
     salesAge: "售龄",
     P_SPU: "P_SPU",
     MID: "MID",
+    designNumber: "设计号",
     itemNumber: "货号",
     styleNumber: "款号",
     picture: "图片",
@@ -333,7 +519,7 @@ class VipshopGoods {
     if (this.vipshopPrice && this.finalPrice) {
       return +this.vipshopPrice > +this.finalPrice ? "活动中" : "";
     }
-    return "未知";
+    return "(未知)";
   }
   set activityStatus(value) {
     this._activityStatus = value;
@@ -343,7 +529,7 @@ class VipshopGoods {
     if (this.finalPrice && this.lowestPrice) {
       return +this.lowestPrice >= +this.finalPrice ? "" : "是";
     }
-    return "未知";
+    return "(未知)";
   }
   set isPriceBroken(value) {
     this._isPriceBroken = value;
@@ -404,13 +590,6 @@ class VipshopGoods {
     this._profitRate = value;
   }
 
-  get totalSales() {}
-  set totalSales(value) {
-    this._totalSales = value;
-  }
-
-  static styleSalesOfLast7Days(styleNumber) {}
-
   toString() {
     return this.itemNumber;
   }
@@ -420,6 +599,7 @@ class VipshopGoods {
 class ProductPrice {
   static wsName = "商品价格";
   static keyToTitle = {
+    designNumber: "设计号",
     itemNumber: "货号",
     picture: "图片",
     costPrice: "成本价",
@@ -432,6 +612,20 @@ class ProductPrice {
   static data = [];
   static initializeData() {
     this.data = DAO.readWorksheet(this.wsName, this, this.keyToTitle);
+
+    let duplicates = Utility.findDuplicatesByProperty(this.data, "itemNumber");
+    if (duplicates.length != 0) {
+      throw new Error(
+        this.wsName +
+          "中存在重复的货号：【" +
+          duplicates +
+          "】，请核查后重试！",
+      );
+    }
+  }
+
+  toString() {
+    return this.itemNumber;
   }
 }
 
@@ -645,17 +839,6 @@ class PriceCalculator {
   }
 }
 
-//更新记录
-class UpdateRecord {
-  static wsName = "更新记录";
-  static keyToTitle = { wsName: "工作表", updateTime: "更新时间" };
-
-  static data = [];
-  static initializeData() {
-    this.data = DAO.readWorksheet(this.wsName, this, this.keyToTitle);
-  }
-}
-
 class DAO {
   static wbName = "商品运营表【史努比】"; //当前工作薄的名称
 
@@ -736,13 +919,13 @@ class Utility {
       result["+" + thisYear + i.toString().padStart(2, "0")] =
         `'${thisYear}` + i.toString().padStart(2, "0");
     }
-    for (let i = 60; i > 0; i--) {
+    for (let i = 45; i > 0; i--) {
       const date = new Date();
       date.setDate(today.getDate() - i);
       const year = date.getFullYear();
-      const month = date.getMonth() + 1;
-      const day = date.getDate();
-      result[`${year}/${month}/${day}`] = `'${month}/${day}`;
+      const month = String(date.getMonth() + 1).padStart(2, "0");
+      const day = String(date.getDate()).padStart(2, 0);
+      result[`+${year}-${month}-${day}`] = `'${month}/${day}`;
     }
 
     return result;
@@ -754,7 +937,7 @@ class Utility {
 
     data.forEach((item) => {
       let value = item[prop];
-      if (seen.has(value)) {
+      if (seen.has(value) && value) {
         duplicates.push(item);
       } else {
         seen.add(value);
