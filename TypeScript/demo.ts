5e0f39089ca2b4e2b110e806ef3dac119d1f17da
Kong-De-Yi <kdeyi@163.com>
2026-04-19 18:37:11 +0800

    26.04.19 18:37

--- TypeScript/demo.ts
+++ TypeScript/demo.ts
@@ -1,5 +1,17 @@
-interface SquareConfig {
-  color?: string;
-  width?: number;
-  [propName: string]: unknown;
+// 这是一个测试
+function myfun(s: string) {
+  return 0;
 }
+
+class ProductStyle {
+  name: string = "Hello World";
+  age: number = 0;
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
+const ps = new ProductStyle("xiaobai", 23);
+
+console.log(ps);
